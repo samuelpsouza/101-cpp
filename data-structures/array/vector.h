1ad893ae5e0405d426812a7594f8c7a97f47599b
Samuel Souza <samuelsouza.contact@gmail.com>
2024-03-29 22:39:48 -0300

    implementing essential data structures in C

---
+++ data-structures/array/vector.h
@@ -0,0 +1,19 @@
+/*
+ * Return the number of items
+ */
+
+int size();
+
+/*
+ * Return the number of items it can hold 
+ */
+
+int capacity();
+
+/*
+ * Return true if it holds any item 
+ */
+
+bool is_empty();
+
+at(int index);
