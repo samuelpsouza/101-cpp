257345ade23224820ec4ac1a89408d1308ac86aa
Samuel Souza <samuelsouza.contact@gmail.com>
2024-03-29 23:25:22 -0300

    basic structs

--- src/basic-c/structs.c
+++ src/basic-c/structs.c
@@ -6,13 +6,17 @@ struct point {
   float y;
 };
 
+typedef struct point Point;
+typedef struct point *PPoint;
+
 int main(int argc, char *argv[])
 {
-  struct point p;
+  // struct point p;
+  Point p;
 
   printf("Type the point coordinates (x,y)\n");
   scanf("%f %f", &p.x, &p.y);
-  printf("The point is (%.2f,%.2f)\n", p.x, p.y);
+  printf("The point is (%.2f, %.2f)\n", p.x, p.y);
 
   return EXIT_SUCCESS;
 }
