674656e4dbf7aa722e41403279d3f53a6b4bc721
Samuel Souza <ssouza.himself@gmail.com>
2024-04-06 02:19:52 -0300

    implementing multi-level pointers

--- src/basic-c/pointers.c
+++ src/basic-c/pointers.c
@@ -22,12 +22,27 @@ int operator(int a, int b, int (*f)(int, int))
 
 int main(int argc, char const *argv[])
 {
+    printf("################# Double Pointers #################\n\n\n");
+
     int a = 2, b = 2;
 
     int sum_response = operator(a, b, &sum);
     int subtraction_response = operator(a, b, &subtraction);
     int multiplication_response = operator(a, b, &multiplication);
-    
-    printf("The code is %d%d%d\n", sum_response, subtraction_response, multiplication_response);    
+
+    printf("The code is %d%d%d\n\n\n", sum_response, subtraction_response, multiplication_response);
+
+    printf("################# Multi-Level Pointers #################\n\n\n");
+
+    int value = 10;
+    int *ptr_level_one;
+    int **ptr_level_two;
+    int ***ptr_level_three;
+
+    ptr_level_one = &value;
+    ptr_level_two = &ptr_level_one;
+    ptr_level_three = &ptr_level_two;
+
+    printf("The value is %d", ***ptr_level_three);
     return 0;
 }
