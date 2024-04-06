d658f8b8d2fb188e3a6ce18a937f3a2f3f1d7cdf
Samuel Souza <ssouza.himself@gmail.com>
2024-04-06 00:31:45 -0300

    playing with primes

--- src/data-structures/array/sieve_eratosthenes.c
+++ src/data-structures/array/sieve_eratosthenes.c
@@ -2,34 +2,32 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-
 int main(int argc, char *argv[])
 {
 
   int n = 16;
   bool primes[n + 1];
-  
-  for(int i = 2; i <= n; i++)
+
+  for (int i = 2; i <= n; i++)
   {
     primes[i] = true;
   }
 
-  for (int divisor = 2;divisor * divisor <= n;divisor++) 
+  for (int divisor = 2; divisor * divisor <= n; divisor++)
   {
-    if (primes[divisor]) 
+    if (primes[divisor])
     {
-      for (int i = 2 * divisor;i <= n; i = i + divisor) 
+      for (int i = 2 * divisor; i <= n; i = i + divisor)
       {
         primes[i] = false;
-	printf("Excluding %d\n", i);
+        printf("Excluding %d\n", i);
       }
     }
   }
 
-  for (int i = 2; i <= n; i++) 
+  for (int i = 2; i <= n; i++)
   {
-    if(primes[i])
+    if (primes[i])
     {
       printf(" %d", i);
     }
