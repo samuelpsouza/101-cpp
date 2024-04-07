c8ed7dc708ec43148000ae0a2a640487ba9ec480
Samuel Souza <ssouza.himself@gmail.com>
2024-04-07 13:17:02 -0300

    change strategy to use internal lists for head and tail

--- src/data-structures/linked-list/linked-list.h
+++ src/data-structures/linked-list/linked-list.h
@@ -6,73 +6,73 @@ typedef struct list List;
   Create a empty linked list
 */
 
-List *new_list(void);
-void free_list(List *list);
+List *new_list();
+void free_list();
 
 /*
   Returns the number of data elements in the list. O(n)
 */
-int size(List *list) ;
+int size() ;
 
 /*
   Returns true if empty
 */
-bool empty(List *list);
+bool empty();
 
 /*
   Returns the value of the nth item (starting at 0 for first)
 */
-int value_at(List *list, int index);
+int value_at(int index);
 
 /*
   Adds an item to the front of the list
 */
-void push_front(List *list, int info);
+void push_front(int info);
 
 /*
   Remove the front item and return its value
 */
-int pop_front(List *list);
+int pop_front();
 
 /*
   Adds an item at the end
 */
-void push_back(List *list, int info);
+void push_back(int info);
 
 /*
   Removes end item and returns its value
 */
-int pop_back(List *list);
+int pop_back();
 
 /*
   Get the value of the front item
 */
-int front(List *list);
+int front();
 
 /*
   Get the value of the end item
 */
-int back(List *list);
+int back();
 
 /*
   Insert value at index, so the current item at that index is pointed to by the new item at the index
 */
-void insert(List *list, int index, int value);
+void insert(int index, int value);
 
 /*
   Removes node at given index
 */
-void erase(List *list, int index); 
+void erase(int index); 
 
 /*
   Returns the value of the node at the nth position from the end of the list
 */
-int value_n_from_end(List *list, int position);
+int value_n_from_end(int position);
 
 /*
   Reverses the list
 */
-void reverse(List *list);
+void reverse();
 
 /*
   Removes the first item in the list with this value
