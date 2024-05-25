a35f88126a07ab94ba3df78b8781f718b0983c06
Samuel <ssouza@that.dev>
2024-05-25 15:53:56 -0300

    implementing binary tree

---
+++ src/data-structures/tree/binary-tree.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <tree.h>
+
+typedef struct tree
+{
+    char info;
+    struct tree* left;
+    struct tree* right;
+} Tree;
+
+Tree* new_empty_tree() {
+    return NULL;
+}
+
+Tree* new_tree(char c, Tree* left, Tree* right){
+    Tree* t = (Tree*)malloc(sizeof(Tree));
+    t->info = c;
+    t->left = left;
+    t->right = right;
+
+    return t;
+}
+
+Tree* free_tree(Tree* t) {
+    if(!is_empty(t)){
+        free_tree(t->left);
+        free_tree(t->right);
+        free(t);
+    }
+
+    return NULL;
+}
+
+bool is_empty(Tree* t) {
+    return t == NULL;
+}
+
+bool is_in_tree(Tree* t, char c) {
+    if(is_empty(t)){
+        return false;
+    }
+
+    return t->info == c || is_in_tree(t->left, c) || is_in_tree(t->right, c);
+}
+
+void print_tree(Tree* t) {
+    printf("<");
+
+    if(!is_empty(t)){
+        printf("%c ", t->info);
+        print_tree(t->left);
+        print_tree(t->right);
+    }
+
+    printf(">");
+}
--- src/data-structures/tree/tree.h
+++ src/data-structures/tree/tree.h
@@ -5,6 +5,6 @@ typedef struct tree Tree;
 Tree* new_empty_tree(void);
 Tree* new_tree(char c, Tree* left, Tree* right);
 Tree* free_tree(Tree* t);
-bool empty_tree(Tree* t);
-bool is_int_tree(Tree* t);
+bool is_empty(Tree* t);
+bool is_in_tree(Tree* t, char c);
 void print_tree(Tree* t);
