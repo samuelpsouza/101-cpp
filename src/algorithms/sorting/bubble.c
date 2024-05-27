8b63100584cd486f7312ab62d4f2184dc920bb6c
Samuel <ssouza@that.dev>
2024-05-27 13:21:34 -0300

    implementing bubble sorting

--- src/algorithms/sorting/bubble.c
+++ src/algorithms/sorting/bubble.c
@@ -1,37 +1,73 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-const int SIZE = 8;
+const int MAX_SIZE = 8;
 
-int main(int argc, char const *argv[])
+
+static bool compare(int a, int b)
 {
-    int a[] = {25,48,37,12,57,86,33,92};
+    return a > b;
+}
 
-    for (int i = 0; i < SIZE; i++)
+void buble(int size, int *a)
+{
+    for (int i = 0; i < size; i++)
     {
         int swap = 0;
-        for (int j = 0; j < SIZE - 1; j++)
+        for (int j = 0; j < size - 1; j++)
         {
-            if(a[j] > a[j+1]) {
+            if (compare(a[j] > a[j + 1]))
+            {
                 int temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
 
-                swap+=1;
+                swap += 1;
             }
         }
 
-        if(swap == 0) {
-            break;
+        if (swap == 0)
+        {
+            return;
         }
-        
+    }
+}
+
+void bubble_rec(int size, int *a)
+{
+    int swap = 0;
+    for (int j = 0; j < size - 1; j++)
+    {
+        if (compare(a[j], a[j + 1]))
+        {
+            int temp = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = temp;
+
+            swap += 1;
+        }
     }
 
-    for (int i = 0; i < SIZE; i++)
+    if (swap != 0)
+    {
+        bubble_rec(size - 1, a);
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int a[] = {25, 48, 37, 12, 57, 86, 33, 92};
+    // buble(MAX_SIZE, a);
+    bubble_rec(MAX_SIZE, a);
+    //bubble_gen(MAX_SIZE, a);
+
+    for (int i = 0; i < MAX_SIZE; i++)
     {
         printf("%d ", a[i]);
     }
-    
+
     printf("\n");
-    
+
     return 0;
 }
